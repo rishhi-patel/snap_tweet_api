a2d684dfb9677840a92206c1b69250b1fdafcb9c
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2025-03-31 00:38:03 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import cors from "cors"
 import dotenv from "dotenv"
-import express, { Express } from "express"
+import express, { Express, Response } from "express"
 import authRoutes from "./routes/authRoutes"
 import tweetRoutes from "./routes/tweetRoutes"
 
@@ -10,6 +10,10 @@ const app: Express = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.get("/", (_, res: Response) => {
+  res.send("API is live")
+})
 
 app.use("/api/auth", authRoutes)
 app.use("/api", tweetRoutes)
